861e5dc876d2141cf8a5f9dab6d1d76d12ef6018
Vivek Bharatha <vivek.bharatha@gmail.com>
2017-04-20 00:34:41 +0530

    Folder structure revamped

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import Header from './Header';
+import Home from './Home';
+import React from 'react';
+import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
+
+const mapStateToProps = state => ({appName: state.appName});
+
+class App extends React.Component {
+  render() {
+    return (
+      <div >
+        <Header appName={this.props.appName} /> {/*asdsa*/}
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+export default connect(mapStateToProps, () => ({}))(App);
