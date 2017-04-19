2bf5ecd73e9284a51bbf05df061f9215ff5c3b71
Vivek Bharatha <vivek.bharatha@gmail.com>
2017-04-20 00:34:04 +0530

    Folder structure revamped and added login route

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 import './index.css';
 import { Provider } from 'react-redux';
+import { Router, Route, Switch } from 'react-router';
+import createBrowserHistory from 'history/createBrowserHistory';
 
+const history = createBrowserHistory();
+
+import App from './components/App';
+import Home from './components/Home';
+import Login from './components/Login';
 import store from './store';
 
 ReactDOM.render((
   <Provider store={ store }>
-    <App />
+    <App>
+      <Router history={history}>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/login" component={Login} />
+        </Switch>
+      </Router>
+    </App>
   </Provider>
 ), document.getElementById('root'));
