d6bd08a3319b3ed467dc51778f840cf73040dc76
Vivek Bharatha <vivek.bharatha@gmail.com>
2017-04-21 18:43:06 +0530

    Link withRouter added to close functionality

--- src/components/App.js
+++ src/components/App.js
@@ -3,18 +3,19 @@ import Home from './Home';
 import React from 'react';
 import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
+import {withRouter} from 'react-router-dom';
 
 const mapStateToProps = state => ({appName: state.appName});
 
 class App extends React.Component {
   render() {
     return (
-      <div >
-        <Header appName={this.props.appName} /> {/*common header*/}
+      <div>
+        <Header appName={this.props.appName} />
         {this.props.children}
       </div>
     );
   }
 }
 
-export default connect(mapStateToProps, () => ({}))(App);
+export default withRouter(connect(mapStateToProps, () => ({}))(App));
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 import './index.css';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Route, Switch, withRouter } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import createBrowserHistory from 'history/createBrowserHistory';
 
 const history = createBrowserHistory();
@@ -14,17 +14,15 @@ import Home from './components/Home';
 import Login from './components/Login';
 import store from './store';
 
-const appWithRouter = withRouter(App);
-
 ReactDOM.render((
   <Provider store={ store }>
-    <appWithRouter>
       <Router history={history}>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/login" component={Login} />
-        </Switch>
+        <App>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/login" component={Login} />
+          </Switch>
+        </App>
       </Router>
-    </appWithRouter>
   </Provider>
 ), document.getElementById('root'));
